493dffd68cc32487e3e14f770a9ff03f51a80ef0
FreakyYang <qq1358970695@gmail.com>
2017-03-23 00:37:07 +0800

    Add Demo Test

---
+++ YQChartViewDEMO/YQChartViewDEMO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQChartViewDEMO
+//
+//  Created by problemchild on 2017/3/21.
+//  Copyright © 2017年 ProblenChild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YQChartViewDEMO/YQChartViewDEMO/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YQChartViewDEMO
+//
+//  Created by problemchild on 2017/3/21.
+//  Copyright © 2017年 ProblenChild. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
